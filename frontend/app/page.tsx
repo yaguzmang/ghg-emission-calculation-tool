6848fd5b13fd83bf8d3c3b41e3f7a438a61ef746
yaguzmang <alejandroguzman.dev@gmail.com>
2023-04-03 18:43:37 +0200

    Set up Next.js and Tailwind css

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className='bg-green-500'>
+      <h1 className='text-2xl underline'>Tailwind test</h1>
+    </main>
+  )
+}
