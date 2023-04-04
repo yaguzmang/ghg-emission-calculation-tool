d51e44ae601a81a789b2071414d67d342df35771
yaguzmang <alejandroguzman.dev@gmail.com>
2023-04-04 16:46:25 +0200

    Add pre commit hook with eslint rules

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,7 @@
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className='bg-green-500'>
-      <h1 className='text-2xl underline'>Tailwind test</h1>
+    <main className="bg-green-500">
+      <h1 className="text-2xl underline">Tailwind test</h1>
     </main>
   )
 }
