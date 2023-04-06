8fb86c943fdbb2022416416af58f63afc240a45f
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-06 22:29:26 +0300

    Run Prettier

--- backend/src/api/form-section/routes/form-section.ts
+++ backend/src/api/form-section/routes/form-section.ts
@@ -2,6 +2,6 @@
  * form-section router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::form-section.form-section');
+export default factories.createCoreRouter("api::form-section.form-section");
