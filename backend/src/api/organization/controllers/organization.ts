76abc1934b9d7de6c910dccbe5a6e8836df0d092
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-06 21:25:34 +0300

    Find only the user's own organizations

--- backend/src/api/organization/controllers/organization.ts
+++ backend/src/api/organization/controllers/organization.ts
@@ -2,6 +2,28 @@
  * organization controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::organization.organization');
+export default factories.createCoreController(
+  "api::organization.organization",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // List only the authenticated user's own organizations
+
+      const { data, meta } = await super.find(ctx);
+      const user = await strapi.entityService.findOne(
+        "plugin::users-permissions.user",
+        ctx.state.user.id,
+        {
+          populate: { organizations: true },
+        }
+      );
+      const ownOrganizationIds = user.organizations.map((org) => org.id);
+      const filteredData = data.filter((org) =>
+        ownOrganizationIds.includes(org.id)
+      );
+
+      return { data: filteredData, meta };
+    },
+  })
+);
