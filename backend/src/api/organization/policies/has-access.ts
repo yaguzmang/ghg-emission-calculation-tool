73807c1e13754b801d4ba629ef362255e4063d5f
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-06 20:43:59 +0300

    Add error logging

--- backend/src/api/organization/policies/has-access.ts
+++ backend/src/api/organization/policies/has-access.ts
@@ -21,8 +21,12 @@ const hasAccess = async (
   const userId = policyContext.state.user?.id;
 
   // Require login
-  if (userId === undefined) return false;
+  if (userId === undefined) {
+    strapi.log.error("Not logged in");
+    return false;
+  }
 
+  // Require valid user
   const user = await strapi.entityService.findOne(
     "plugin::users-permissions.user",
     userId,
@@ -30,20 +34,31 @@ const hasAccess = async (
       populate: { organizations: true },
     }
   );
-
-  // Require valid user
-  if (!user) return false;
+  if (!user) {
+    strapi.log.error("Not a valid user");
+    return false;
+  }
 
   // Require organizationId
   if (
     !Array.isArray(policyContext.captures) ||
     policyContext.captures.length < 1
-  )
+  ) {
+    strapi.log.error("Organization id could not be determined");
     return false;
+  }
 
+  // Require access to organization
   const organizationId = Number(policyContext.captures[0]);
+  const userHasAccessToOrganization = user.organizations
+    .map((org) => org.id)
+    .includes(organizationId);
+  if (!userHasAccessToOrganization) {
+    strapi.log.error("User does not have access to organization");
+    return false;
+  }
 
-  return user.organizations.map((org) => org.id).includes(organizationId);
+  return true;
 };
 
 export default hasAccess;
