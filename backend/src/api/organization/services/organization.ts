c8698471722f917167b424483d8c891359009785
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-11 06:53:45 +0300

    Run prettier

--- backend/src/api/organization/services/organization.ts
+++ backend/src/api/organization/services/organization.ts
@@ -2,6 +2,6 @@
  * organization service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::organization.organization');
+export default factories.createCoreService("api::organization.organization");
