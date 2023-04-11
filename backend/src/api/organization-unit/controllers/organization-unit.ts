84403f225897db21c4c60e67fee356d08b7fc1cb
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-11 10:03:59 +0300

    List only the authenticated user's organization units

--- backend/src/api/organization-unit/controllers/organization-unit.ts
+++ backend/src/api/organization-unit/controllers/organization-unit.ts
@@ -2,6 +2,30 @@
  * organization-unit controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::organization-unit.organization-unit');
+export default factories.createCoreController(
+  "api::organization-unit.organization-unit",
+  ({ strapi }) => ({
+    async find(ctx) {
+      // List only the authenticated user's own organization units
+
+      const { data, meta } = await super.find(ctx);
+      const user = await strapi.entityService.findOne(
+        "plugin::users-permissions.user",
+        ctx.state.user.id,
+        {
+          populate: { organizations: true },
+        }
+      );
+      const ownOrganizationIds = user.organizations.map((org) => org.id);
+      const filteredData = data.filter((orgUnit) =>
+        ownOrganizationIds.includes(orgUnit.attributes.organization.data.id)
+      );
+
+      meta.pagination.total = filteredData.length;
+
+      return { data: filteredData, meta };
+    },
+  })
+);
