8f0b07067046c799b9a0120d8a6cc2655e9eeb9d
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-17 09:48:26 +0300

    Replace Form with Emission group

---
+++ backend/src/api/emission-group/controllers/emission-group.ts
@@ -0,0 +1,7 @@
+/**
+ * emission-group controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::emission-group.emission-group');
---
+++ backend/src/api/emission-group/services/emission-group.ts
@@ -0,0 +1,7 @@
+/**
+ * emission-group service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::emission-group.emission-group');
