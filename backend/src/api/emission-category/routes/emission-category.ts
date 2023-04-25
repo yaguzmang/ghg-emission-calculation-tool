f3a219e255e7979e600ac8c39133cfed23649ed6
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-25 07:01:42 +0300

    Add emission category collection type

---
+++ backend/src/api/emission-category/routes/emission-category.ts
@@ -0,0 +1,7 @@
+/**
+ * emission-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::emission-category.emission-category');
---
+++ backend/src/api/emission-category/services/emission-category.ts
@@ -0,0 +1,7 @@
+/**
+ * emission-category service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::emission-category.emission-category');
