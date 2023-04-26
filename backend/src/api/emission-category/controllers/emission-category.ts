9c5059bf94999359f5abe7729af7b7a446a68360
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-04-26 07:09:43 +0300

    Populate emission sources to non-English locales

--- backend/src/api/emission-category/controllers/emission-category.ts
+++ backend/src/api/emission-category/controllers/emission-category.ts
@@ -5,5 +5,30 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
-  "api::emission-category.emission-category"
+  "api::emission-category.emission-category",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { data, meta } = await super.findOne(ctx);
+      const { populate } = ctx.query;
+
+      if (
+        data.attributes.locale != "en" &&
+        (populate == "*" || populate == "emissionSources")
+      ) {
+        // Populate emission sources to non-English locales
+        const localizationQuery = {
+          populate: { localizations: { populate: "emissionSources" } },
+        };
+        const localizationCtx = { ...ctx, query: localizationQuery };
+        const { data: localizationData } = await super.findOne(localizationCtx);
+        const { localizations } = localizationData.attributes;
+        const enLocale = localizations.data.find(
+          ({ attributes }) => attributes.locale == "en"
+        );
+        data.attributes.emissionSources = enLocale.attributes.emissionSources;
+      }
+
+      return { data, meta };
+    },
+  })
 );
