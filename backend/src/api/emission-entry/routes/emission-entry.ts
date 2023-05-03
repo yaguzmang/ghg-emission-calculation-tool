d547dc2795f812a6b9c65818d8866f23c25e6cf8
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-05-03 11:29:12 +0300

    Add emission entry collection type

---
+++ backend/src/api/emission-entry/routes/emission-entry.ts
@@ -0,0 +1,7 @@
+/**
+ * emission-entry router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::emission-entry.emission-entry');
---
+++ backend/src/api/emission-entry/services/emission-entry.ts
@@ -0,0 +1,7 @@
+/**
+ * emission-entry service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::emission-entry.emission-entry');
