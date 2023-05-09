475d8339b9294ad792ccd35959c86b07affe31e0
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-05-09 08:31:45 +0300

    Run prettier

--- backend/src/api/emission-entry/controllers/emission-entry.ts
+++ backend/src/api/emission-entry/controllers/emission-entry.ts
@@ -2,6 +2,8 @@
  * emission-entry controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::emission-entry.emission-entry');
+export default factories.createCoreController(
+  "api::emission-entry.emission-entry"
+);
