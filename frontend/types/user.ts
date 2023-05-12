987dc8e53a3df6d9e81e7657c5481c45b36f2cdb
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-12 18:31:10 +0200

    Add types for user, organization and session
    Unfortunately there is no quick fix to avoid getting the
    no-unused-vars for the session type in next-auth, but this
    is not a problem as that file will not have any logic.

---
+++ frontend/types/user.ts
@@ -0,0 +1,13 @@
+import { Organization } from './organization';
+
+export type User = {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  organizations: Organization[];
+};
