192e252cbb5fe68bcea3295ffabf9fa093c1d142
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-05-19 16:14:42 +0300

    Move custom emission source population to emission source group

--- backend/src/api/emission-category/controllers/emission-category.ts
+++ backend/src/api/emission-category/controllers/emission-category.ts
@@ -5,33 +5,5 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
-  "api::emission-category.emission-category",
-  ({ strapi }) => ({
-    async findOne(ctx) {
-      const res = await super.findOne(ctx);
-      if (!res) return null;
-
-      const { data, meta } = res;
-      const { populate } = ctx.query;
-
-      if (
-        data.attributes.locale != "en" &&
-        (populate == "*" || populate == "emissionSources")
-      ) {
-        // Populate emission sources to non-English locales
-        const localizationQuery = {
-          populate: { localizations: { populate: "emissionSources" } },
-        };
-        const localizationCtx = { ...ctx, query: localizationQuery };
-        const { data: localizationData } = await super.findOne(localizationCtx);
-        const { localizations } = localizationData.attributes;
-        const enLocale = localizations.data.find(
-          ({ attributes }) => attributes.locale == "en"
-        );
-        data.attributes.emissionSources = enLocale.attributes.emissionSources;
-      }
-
-      return { data, meta };
-    },
-  })
+  "api::emission-category.emission-category"
 );
--- backend/src/api/emission-source-group/services/emission-source-group.ts
+++ backend/src/api/emission-source-group/services/emission-source-group.ts
@@ -2,6 +2,53 @@
  * emission-source-group service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
+import {
+  ApiServiceEntry,
+  LocalizedApiServiceEntry,
+  ServiceParams,
+} from "../../api.types";
 
-export default factories.createCoreService('api::emission-source-group.emission-source-group');
+interface EmissionSourceGroup extends LocalizedApiServiceEntry {
+  name: string;
+  emissionSourceLabel;
+  emissionSources?: ApiServiceEntry[];
+  emissionCategory?: LocalizedApiServiceEntry;
+  localizations?: EmissionSourceGroup[];
+}
+
+export default factories.createCoreService(
+  "api::emission-source-group.emission-source-group",
+  ({ strapi }) => ({
+    async findOne(id: string, params: ServiceParams) {
+      const res = (await super.findOne(
+        id,
+        params
+      )) as EmissionSourceGroup | null;
+
+      if (!res) return null;
+
+      if (
+        res.locale !== "en" &&
+        ((typeof params.populate === "string" &&
+          ["*", "emissionSources"].includes(params.populate)) ||
+          (Array.isArray(params.populate) &&
+            params.populate.includes("emissionSources")))
+      ) {
+        console.log("custom populate");
+        const { localizations } = (await super.findOne(id, {
+          populate: { localizations: { populate: "emissionSources" } },
+        })) as EmissionSourceGroup;
+
+        const enEmissionSourceGroup = localizations.find(
+          ({ locale }) => locale === "en"
+        );
+        if (enEmissionSourceGroup) {
+          res.emissionSources = enEmissionSourceGroup.emissionSources;
+        }
+      }
+
+      return res;
+    },
+  })
+);
