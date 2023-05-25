d2b0d2ed10e413f4e8a5a9101b126d7ffdc796d0
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-25 18:21:49 +0200

    Add initial batch of icons

---
+++ frontend/components/ui/icons/icons.tsx
@@ -0,0 +1,13 @@
+import Check from './check.svg';
+import ChevronDown from './chevron-down.svg';
+import QuestionMark from './question-mark.svg';
+import Search from './search.svg';
+import X from './x.svg';
+
+export const Icons = {
+  close: X,
+  questionMark: QuestionMark,
+  search: Search,
+  chevronDown: ChevronDown,
+  check: Check,
+};
