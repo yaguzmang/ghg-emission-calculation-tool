a4e00e213da13705786af714713d2544f6d38939
yaguzmang <alejandroguzman.dev@gmail.com>
2023-05-27 17:34:43 +0200

    Add more icons and logo

--- frontend/components/ui/icons/icons.tsx
+++ frontend/components/ui/icons/icons.tsx
@@ -1,7 +1,11 @@
 import Check from './check.svg';
 import ChevronDown from './chevron-down.svg';
+import HelpCircleFilled from './help-circle-filled.svg';
+import NotchDown from './notch-down.svg';
 import QuestionMark from './question-mark.svg';
 import Search from './search.svg';
+import TallinnaTehnikaulikool from './tallinna-tehnikaulikool.svg';
+import UserAvatar from './user-avatar.svg';
 import X from './x.svg';
 
 export const Icons = {
@@ -10,4 +14,8 @@ export const Icons = {
   search: Search,
   chevronDown: ChevronDown,
   check: Check,
+  tallinnaTehnikaulikool: TallinnaTehnikaulikool,
+  userAvatar: UserAvatar,
+  notchDown: NotchDown,
+  helpCircleFilled: HelpCircleFilled,
 };
