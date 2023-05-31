bf023bdc9bf1000f2e3add9e35f14804e5d1e729
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-05-31 12:26:26 +0300

    Populate emission sources for non-English emission categories

--- backend/src/api/emission-category/services/emission-category.ts
+++ backend/src/api/emission-category/services/emission-category.ts
@@ -4,6 +4,52 @@
 
 import { factories } from "@strapi/strapi";
 
+import {
+  ApiServiceEntry,
+  LocalizedApiServiceEntry,
+  ServiceParams,
+} from "../../api.types";
+
+interface EmissionCategory extends LocalizedApiServiceEntry {
+  title: string;
+  description?: string;
+  emissionGroup?: LocalizedApiServiceEntry;
+  emissionSources?: ApiServiceEntry[];
+  localizations?: EmissionCategory[];
+}
+
 export default factories.createCoreService(
-  "api::emission-category.emission-category"
+  "api::emission-category.emission-category",
+  ({ strapi }) => ({
+    async findOne(id: string, params: ServiceParams) {
+      const res = (await super.findOne(id, params)) as EmissionCategory | null;
+
+      if (!res) return null;
+
+      console.log(params.populate);
+
+      if (
+        (res.locale !== "en" &&
+          ((typeof params.populate === "string" &&
+            ["*", "emissionSources"].includes(params.populate)) ||
+            (Array.isArray(params.populate) &&
+              params.populate.includes("emissionSources")))) ||
+        (typeof params.populate === "object" &&
+          Object.keys(params.populate).includes("emissionSources"))
+      ) {
+        const { localizations } = (await super.findOne(id, {
+          populate: { localizations: { populate: params.populate } },
+        })) as EmissionCategory;
+
+        const enEmissionCategory = localizations.find(
+          ({ locale }) => locale === "en"
+        );
+        if (enEmissionCategory) {
+          res.emissionSources = enEmissionCategory.emissionSources;
+        }
+      }
+
+      return res;
+    },
+  })
 );
--- backend/src/api/emission-source-group/services/emission-source-group.ts
+++ backend/src/api/emission-source-group/services/emission-source-group.ts
@@ -3,52 +3,7 @@
  */
 
 import { factories } from "@strapi/strapi";
-import {
-  ApiServiceEntry,
-  LocalizedApiServiceEntry,
-  ServiceParams,
-} from "../../api.types";
-
-interface EmissionSourceGroup extends LocalizedApiServiceEntry {
-  name: string;
-  emissionSourceLabel;
-  emissionSources?: ApiServiceEntry[];
-  emissionCategory?: LocalizedApiServiceEntry;
-  localizations?: EmissionSourceGroup[];
-}
 
 export default factories.createCoreService(
-  "api::emission-source-group.emission-source-group",
-  ({ strapi }) => ({
-    async findOne(id: string, params: ServiceParams) {
-      const res = (await super.findOne(
-        id,
-        params
-      )) as EmissionSourceGroup | null;
-
-      if (!res) return null;
-
-      if (
-        res.locale !== "en" &&
-        ((typeof params.populate === "string" &&
-          ["*", "emissionSources"].includes(params.populate)) ||
-          (Array.isArray(params.populate) &&
-            params.populate.includes("emissionSources")))
-      ) {
-        console.log("custom populate");
-        const { localizations } = (await super.findOne(id, {
-          populate: { localizations: { populate: "emissionSources" } },
-        })) as EmissionSourceGroup;
-
-        const enEmissionSourceGroup = localizations.find(
-          ({ locale }) => locale === "en"
-        );
-        if (enEmissionSourceGroup) {
-          res.emissionSources = enEmissionSourceGroup.emissionSources;
-        }
-      }
-
-      return res;
-    },
-  })
+  "api::emission-source-group.emission-source-group"
 );
