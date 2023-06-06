b9b76e40748d23b4da941e06a9e9864beb4ec4c5
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-06-06 11:46:37 +0300

    Validate get emission categories with emissions request

--- backend/src/api/emission-category/controllers/emission-category.ts
+++ backend/src/api/emission-category/controllers/emission-category.ts
@@ -3,12 +3,28 @@
  */
 
 import { factories } from "@strapi/strapi";
+import utils from "@strapi/utils";
+import * as yup from "yup";
+import { validate } from "../../../services/utils";
 
 export default factories.createCoreController(
   "api::emission-category.emission-category",
   ({ strapi }) => ({
     async findWithEmissions(ctx) {
       console.log("findWithEmissions");
+
+      const querySchema = yup.object({
+        locale: yup.string().required(),
+        reportingPeriod: yup.number().required(),
+      });
+
+      const { locale, reportingPeriod } = await validate(
+        ctx.query,
+        querySchema,
+        utils.errors.ValidationError
+      );
+
+      console.log(locale, reportingPeriod);
       return;
     },
   })
