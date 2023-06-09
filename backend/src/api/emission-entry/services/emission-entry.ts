28e345fe56b708e2f14d7e41a597ff0aa280d66a
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-06-09 14:50:15 +0300

    Add services

--- backend/src/api/emission-entry/services/emission-entry.ts
+++ backend/src/api/emission-entry/services/emission-entry.ts
@@ -3,14 +3,23 @@
  */
 
 import { factories } from "@strapi/strapi";
+import { GenericService } from "@strapi/strapi/lib/core-api/service";
+import * as yup from "yup";
+import { EmissionEntry } from "..";
+import { EmissionFactorDatumService } from "../../emission-factor-datum/services/emission-factor-datum";
 
-export default factories.createCoreService(
+export type EmissionEntryService = GenericService & {
+  isAllowedForUser(emissionEntryId: number, userId: number): Promise<boolean>;
+  findWithEmissions(
+    reportingPeriodId: number,
+    locale: string
+  ): Promise<EmissionEntry[]>;
+};
+
+export default factories.createCoreService<EmissionEntryService>(
   "api::emission-entry.emission-entry",
   ({ strapi }) => ({
-    async isAllowedForUser(
-      emissionEntryId: number,
-      userId: number
-    ): Promise<boolean> {
+    async isAllowedForUser(emissionEntryId, userId) {
       const user = await strapi.entityService.findOne(
         "plugin::users-permissions.user",
         userId,
@@ -31,6 +40,52 @@ export default factories.createCoreService(
       );
 
       return ownEmissionEntries.some((unit) => unit.id === emissionEntryId);
+    },
+
+    async findWithEmissions(reportingPeriodId, locale = "en") {
+      const getEmissionEntries: EmissionEntry[] = strapi.entityService.findMany(
+        "api::emission-entry.emission-entry",
+        {
+          filters: {
+            reportingPeriod: reportingPeriodId,
+          },
+          populate: ["emissionSource"],
+        }
+      );
+
+      const getEmissionFactorDatum = strapi
+        .service<EmissionFactorDatumService>(
+          "api::emission-factor-datum.emission-factor-datum"
+        )
+        .findOneByReportingPeriod(reportingPeriodId, { locale });
+
+      const [emissionEntries, emissionFactorDatum] = await Promise.all([
+        getEmissionEntries,
+        getEmissionFactorDatum,
+      ]);
+
+      const json = await strapi
+        .service<EmissionFactorDatumService>(
+          "api::emission-factor-datum.emission-factor-datum"
+        )
+        .validateJson(emissionFactorDatum.json);
+
+      const entriesWithEmissions = emissionEntries.map((entry) => {
+        const q = entry.quantity;
+        const factors =
+          json.emission_sources[entry.emissionSource.apiId]?.factors;
+        const emissions = {
+          direct: q * (factors?.direct?.value ?? 0),
+          indirect: q * (factors?.indirect?.value ?? 0),
+          biogenic: q * (factors?.biogenic?.value ?? 0),
+        };
+        return {
+          ...entry,
+          emissions,
+        };
+      });
+
+      return entriesWithEmissions;
     },
   })
 );
--- backend/src/api/emission-factor-datum/services/emission-factor-datum.ts
+++ backend/src/api/emission-factor-datum/services/emission-factor-datum.ts
@@ -3,7 +3,122 @@
  */
 
 import { factories } from "@strapi/strapi";
+import { GenericService } from "@strapi/strapi/lib/core-api/service";
+import utils from "@strapi/utils";
+import * as yup from "yup";
+import { parse, format } from "date-fns";
+import { EmissionFactorDatum } from "..";
+import { ReportingPeriod } from "../../reporting-period";
 
-export default factories.createCoreService(
-  "api::emission-factor-datum.emission-factor-datum"
+const factorSchema = yup.object({
+  value: yup.number(),
+  data_source: yup.object({
+    description: yup.string(),
+    url: yup.string(),
+  }),
+});
+
+const emissionSourceValueSchema = yup.object({
+  label: yup.string().required(),
+  unit: yup.string().required(),
+  factors: yup.object({
+    direct: factorSchema,
+    indirect: factorSchema,
+    biogenic: factorSchema,
+  }),
+});
+
+type JsonEmissionSourceValue = yup.InferType<typeof emissionSourceValueSchema>;
+
+interface Json {
+  emission_sources: {
+    [key: string]: JsonEmissionSourceValue;
+  };
+}
+
+export type EmissionFactorDatumService = GenericService & {
+  findOneByReportingPeriod(
+    reportingPeriodId: number,
+    params: object
+  ): Promise<EmissionFactorDatum>;
+  validateJson(json: unknown): Promise<Json>;
+};
+
+const { ApplicationError, NotFoundError } = utils.errors;
+
+export default factories.createCoreService<EmissionFactorDatumService>(
+  "api::emission-factor-datum.emission-factor-datum",
+  ({ strapi }) => ({
+    async findOneByReportingPeriod(reportingPeriodId, params) {
+      const reportingPeriod: ReportingPeriod =
+        await strapi.entityService.findOne(
+          "api::reporting-period.reporting-period",
+          reportingPeriodId,
+          {
+            populate: { organization: { populate: ["emissionFactorDataset"] } },
+          }
+        );
+
+      if (!reportingPeriodId) {
+        throw new NotFoundError("reporting period not found");
+      }
+
+      const { organization, endDate: endDateString } = reportingPeriod;
+
+      if (!organization) {
+        throw new ApplicationError(
+          "no organization assigned to reporting period"
+        );
+      }
+
+      const { emissionFactorDataset } = organization;
+
+      if (!emissionFactorDataset) {
+        throw new ApplicationError(
+          "no emission factor dataset assigned to organization"
+        );
+      }
+
+      const endDate = parse(endDateString, "y-MM-dd", new Date());
+      const year = format(endDate, "y");
+
+      const emissionFactorData: EmissionFactorDatum[] =
+        await strapi.entityService.findMany(
+          "api::emission-factor-datum.emission-factor-datum",
+          {
+            filters: {
+              dataset: emissionFactorDataset,
+              year,
+            },
+            sort: { updatedAt: "desc" },
+            ...params,
+          }
+        );
+
+      if (emissionFactorData.length < 1) {
+        throw new NotFoundError("emission factor data not found");
+      }
+
+      return emissionFactorData[0];
+    },
+
+    async validateJson(json) {
+      const jsonSchema = yup.object({
+        emission_sources: yup.lazy((obj) => {
+          if (!obj || typeof obj !== "object" || Array.isArray(obj)) {
+            return yup.object().required();
+          }
+          const entries = Object.entries(obj).map(([key]) => {
+            return [key, emissionSourceValueSchema];
+          });
+
+          return yup.object(Object.fromEntries(entries));
+        }),
+      });
+
+      const validatedJson = (await jsonSchema.validate(json)) as Json;
+
+      return validatedJson;
+    },
+  })
 );
