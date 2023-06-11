b610bce071740c51d05345d03cb40e664c11af5e
yaguzmang <alejandroguzman.dev@gmail.com>
2023-06-11 21:30:35 +0200

    Add Emissions icons

--- frontend/components/ui/icons/icons.tsx
+++ frontend/components/ui/icons/icons.tsx
@@ -1,11 +1,14 @@
 import Check from './check.svg';
 import ChevronDown from './chevron-down.svg';
+import DirectEmissions from './DirectEmissions.svg';
 import HelpCircleFilled from './help-circle-filled.svg';
+import IndirectEmissions from './IndirectEmissions.svg';
 import NotchDown from './notch-down.svg';
 import QuestionMark from './question-mark.svg';
 import Search from './search.svg';
 import TallinnaTehnikaulikool from './tallinna-tehnikaulikool.svg';
 import UserAvatar from './user-avatar.svg';
+import ValueChainEmissions from './ValueChainEmissions.svg';
 import X from './x.svg';
 
 export const Icons = {
@@ -18,4 +21,7 @@ export const Icons = {
   UserAvatar,
   NotchDown,
   HelpCircleFilled,
+  DirectEmissions,
+  IndirectEmissions,
+  ValueChainEmissions,
 };
