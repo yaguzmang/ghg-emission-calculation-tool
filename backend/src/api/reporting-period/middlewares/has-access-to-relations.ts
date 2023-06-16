cd10c323e93a569d6dc6feda197c229ae0884851
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-06-16 12:24:36 +0300

    Validate create reporting period request

--- backend/src/api/reporting-period/middlewares/has-access-to-relations.ts
+++ backend/src/api/reporting-period/middlewares/has-access-to-relations.ts
@@ -3,7 +3,12 @@
  */
 
 import { Strapi } from "@strapi/strapi";
+import utils from "@strapi/utils";
 import * as yup from "yup";
+import { validate } from "../../../services/utils";
+import { OrganizationService } from "../../organization/services/organization";
+
+const { ValidationError } = utils.errors;
 
 export default (config, { strapi }: { strapi: Strapi }) => {
   // Add your own logic here.
@@ -15,6 +20,17 @@ export default (config, { strapi }: { strapi: Strapi }) => {
         organization: yup.number().required(),
       }),
     });
+
+    const {
+      data: { organization },
+    } = await validate(ctx.request.body, relationSchema, ValidationError);
+
+    const userHasAccessToOrganization = await strapi
+      .service<OrganizationService>("api::organization.organization")
+      ?.isAllowedForUser(organization, ctx.state.user.id);
+
+    if (!userHasAccessToOrganization)
+      return ctx.forbidden("forbidden organization id");
 
     await next();
   };
