c8f71f4d24484f5c1c944320f86087a6731fbca2
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-06-21 11:47:48 +0300

    Run prettier

--- backend/src/api/organization-divider/controllers/organization-divider.ts
+++ backend/src/api/organization-divider/controllers/organization-divider.ts
@@ -2,6 +2,8 @@
  * organization-divider controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::organization-divider.organization-divider');
+export default factories.createCoreController(
+  "api::organization-divider.organization-divider"
+);
