693631e4d9eba2c66dc4ac697727af663970e553
yaguzmang <alejandroguzman.dev@gmail.com>
2023-07-28 18:50:05 +0200

    Update site header

--- frontend/components/site-header.tsx
+++ frontend/components/site-header.tsx
@@ -28,12 +28,12 @@ export function SiteHeader() {
     shouldRenderHeader && (
       <header className="top-0 z-40 w-full bg-primary drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
         <div className="ml-0 mr-0 flex h-10 items-center bg-primary-foreground">
-          <div className="relative flex h-full min-w-[180px] items-center justify-center bg-taltech-green">
+          <div className="relative flex h-full min-w-[180px] items-center justify-center bg-green">
             <h4 className="font-extrabold text-primary-foreground px-5">
               {appName}
             </h4>
             <div className="absolute top-full">
-              <Icons.NotchDown className="h-[7px] w-[16px] text-taltech-green" />
+              <Icons.NotchDown className="h-[7px] w-[16px] text-green" />
             </div>
           </div>
           <div className="mr-8 flex flex-1 items-center justify-end text-primary">
