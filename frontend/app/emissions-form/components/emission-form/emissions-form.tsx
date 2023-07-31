956f9fa5195ac9d515279e9502821c0497ec0604
yaguzmang <alejandroguzman.dev@gmail.com>
2023-07-31 21:57:30 +0200

    Add support for EEIO sources using euro unit

--- frontend/app/emissions-form/components/emission-form/emissions-form.tsx
+++ frontend/app/emissions-form/components/emission-form/emissions-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
@@ -221,6 +221,28 @@ export default function EmissionsForm({
     form.getValues('emissionSource'),
   );
 
+  const isSourceEEIO = selectedEmissionSource?.unit === '€';
+
+  useEffect(() => {
+    if (isSourceEEIO) {
+      form.setValue('tier', 0);
+      form.trigger('tier');
+    } else if (formType === 'edit' && emissionEntry?.attributes?.tier === 0) {
+      // The emission entry tier is 0 but the source is not EEIO,
+      // so the Tier has to be either 1, 2, or 3. To enforce that, set to -1
+      // so the validation fails and the user needs to choose a new tier.
+      form.setValue('tier', -1);
+    } else if (formType === 'edit' && emissionEntry?.attributes?.tier !== 0) {
+      // User had switched to EEIO source, which set the tier to 0, so restore
+      // the original value.
+      form.setValue('tier', emissionEntry?.attributes?.tier ?? -1);
+    } else {
+      // User switched between EEIO and non EEIO source, so clean up the selected
+      // tier 0.
+      form.setValue('tier', -1);
+    }
+  }, [isSourceEEIO, emissionEntry?.attributes?.tier, form, formType]);
+
   return (
     <FormProvider {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col">
@@ -351,60 +373,85 @@ export default function EmissionsForm({
             {t('dashboard.form.emissionEntry.accuracy')}
           </span>
 
-          <Controller
-            defaultValue={
-              formType === 'edit' ? emissionEntry?.attributes?.tier : undefined
-            }
-            control={form.control}
-            name="tier"
-            render={({ field }) => (
-              // TODO: Add tier 0
-              <ToggleGroup
-                defaultValue={
-                  formType === 'edit'
-                    ? emissionEntry?.attributes?.tier?.toString()
-                    : undefined
+          {isSourceEEIO ? (
+            <ToggleGroup value="0" type="single" disabled>
+              <ToggleGroupItem
+                value="0"
+                className={
+                  form.formState?.errors?.tier?.message !== undefined
+                    ? 'border-destructive'
+                    : ''
                 }
-                ref={field.ref}
-                type="single"
-                onValueChange={(selectedValue: string) => {
-                  const selectedTier = parseInt(selectedValue, 10);
-                  field.onChange(selectedTier);
-                }}
               >
-                <ToggleGroupItem
-                  value="1"
-                  className={
-                    form.formState?.errors?.tier?.message !== undefined
-                      ? 'border-destructive'
-                      : ''
+                <span className="px-3">{`${t(
+                  'dashboard.form.emissionEntry.tier',
+                )} 0`}</span>
+              </ToggleGroupItem>
+            </ToggleGroup>
+          ) : (
+            <Controller
+              defaultValue={
+                formType === 'edit'
+                  ? emissionEntry?.attributes?.tier
+                  : undefined
+              }
+              control={form.control}
+              name="tier"
+              render={({ field }) => (
+                <ToggleGroup
+                  defaultValue={
+                    formType === 'edit'
+                      ? emissionEntry?.attributes?.tier?.toString()
+                      : undefined
                   }
+                  ref={field.ref}
+                  type="single"
+                  onValueChange={(selectedValue: string) => {
+                    const selectedTier = parseInt(selectedValue, 10);
+                    field.onChange(selectedTier);
+                  }}
                 >
-                  <span className="px-3">Tier 1</span>
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  value="2"
-                  className={
-                    form.formState?.errors?.tier?.message !== undefined
-                      ? 'border-destructive'
-                      : ''
-                  }
-                >
-                  <span className="px-3">Tier 2</span>
-                </ToggleGroupItem>
-                <ToggleGroupItem
-                  value="3"
-                  className={
-                    form.formState?.errors?.tier?.message !== undefined
-                      ? 'border-destructive'
-                      : ''
-                  }
-                >
-                  <span className="px-3">Tier 3</span>{' '}
-                </ToggleGroupItem>
-              </ToggleGroup>
-            )}
-          />
+                  <ToggleGroupItem
+                    value="1"
+                    className={
+                      form.formState?.errors?.tier?.message !== undefined
+                        ? 'border-destructive'
+                        : ''
+                    }
+                  >
+                    <span className="px-3">{`${t(
+                      'dashboard.form.emissionEntry.tier',
+                    )} 1`}</span>
+                  </ToggleGroupItem>
+                  <ToggleGroupItem
+                    value="2"
+                    className={
+                      form.formState?.errors?.tier?.message !== undefined
+                        ? 'border-destructive'
+                        : ''
+                    }
+                  >
+                    <span className="px-3">{`${t(
+                      'dashboard.form.emissionEntry.tier',
+                    )} 2`}</span>
+                  </ToggleGroupItem>
+                  <ToggleGroupItem
+                    value="3"
+                    className={
+                      form.formState?.errors?.tier?.message !== undefined
+                        ? 'border-destructive'
+                        : ''
+                    }
+                  >
+                    <span className="px-3">{`${t(
+                      'dashboard.form.emissionEntry.tier',
+                    )} 3`}</span>{' '}
+                  </ToggleGroupItem>
+                </ToggleGroup>
+              )}
+            />
+          )}
+
           {form.formState?.errors?.tier?.message !== undefined && (
             <span className="text-sm text-destructive basis-full">
               {form.formState.errors.tier.message}
