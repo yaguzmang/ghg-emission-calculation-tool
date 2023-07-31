9e1705841ebc10c83343d38dfdccaf084347eaf9
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-07-31 11:36:12 +0300

    Add quantityLabel to types

--- backend/src/api/emission-source-group/index.ts
+++ backend/src/api/emission-source-group/index.ts
@@ -3,5 +3,6 @@ import { LocalizedApiServiceEntry } from "../api.types";
 export interface EmissionSourceGroup extends LocalizedApiServiceEntry {
   name: string;
   emissionSourceLabel: string;
+  quantityLabel?: string;
   localizations?: EmissionSourceGroup[];
 }
