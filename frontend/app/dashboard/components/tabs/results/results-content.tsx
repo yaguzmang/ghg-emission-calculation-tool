6ed0fa5d1e6cdb2921db846174308974ce80a84d
ayudame.servicio <ayudame.servicio@gmail.com>
2023-08-21 13:28:50 +0200

    Add tooltip for accuracy of results

--- frontend/app/dashboard/components/tabs/results/results-content.tsx
+++ frontend/app/dashboard/components/tabs/results/results-content.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useTranslation } from 'react-i18next';
+
 import { EmissionCategoriesLollipopChart } from './components/emission-categories-lollipop-chart';
 import { GHGEmissionsBetweenOrganizationalUnitsContainer } from './components/ghg-emissions-between-organizational-units/organizational-units-container';
 import { OrganizationUnitBannerContainer } from './components/ghg-emissions-of-each-organization-unit/organization-unit-banner-container';
@@ -7,6 +9,13 @@ import { OrganizationUnitBannerContainer } from './components/ghg-emissions-of-e
 import EmissionsByScopeTable from '@/components/emission-results/emissions-by-scope-table';
 import { EmissionsSummaryPanel } from '@/components/emissions-summary-panel/emissions-summary-panel';
 import { OrganizationPeriodForm } from '@/components/organization-period-form/organization-period-form';
+import { Button } from '@/components/ui/button';
+import {
+  ArrowReadMore,
+  Popover,
+  PopoverContentReadMore,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import {
   useSelectedLocale,
   useSelectedOrganizationId,
@@ -14,6 +23,7 @@ import {
 } from '@/redux/store/ui/shared';
 
 export function ResultsTabContent() {
+  const { t } = useTranslation();
   const selectedLocale = useSelectedLocale();
   const selectedeportingPeriodId = useSelectedReportingPeriodId('results');
   const selectedOrganizationId = useSelectedOrganizationId('results');
@@ -27,6 +37,19 @@ export function ResultsTabContent() {
             locale={selectedLocale}
             includeAccuracy
           />
+          <div className="ml-auto -mt-4">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="link" size="fit" type="button">
+                  {t('results.accuracy.readMore')}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContentReadMore side="bottom" sideOffset={12}>
+                <p>{t('results.accuracy.readMore.content')}</p>
+                <ArrowReadMore />
+              </PopoverContentReadMore>
+            </Popover>
+          </div>
         </div>
         {selectedeportingPeriodId && selectedLocale && (
           <div className="border border-primary rounded-[2px] px-2 py-8 sm:px-8 w-full h-fit flex flex-row flex-wrap md:flex-nowrap gap-2">
