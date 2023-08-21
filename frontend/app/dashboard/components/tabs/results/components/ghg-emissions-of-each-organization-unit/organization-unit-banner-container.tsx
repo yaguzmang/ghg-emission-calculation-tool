c0edbf829bae05cba7c1511e0561c349d66a4c12
ayudame.servicio <ayudame.servicio@gmail.com>
2023-08-21 13:17:46 +0200

    Add title for GHG Emissions of each organizational unit

--- frontend/app/dashboard/components/tabs/results/components/ghg-emissions-of-each-organization-unit/organization-unit-banner-container.tsx
+++ frontend/app/dashboard/components/tabs/results/components/ghg-emissions-of-each-organization-unit/organization-unit-banner-container.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useTranslation } from 'react-i18next';
+
 import { OrganizationUnitBanner } from './organization-unit-banner';
 
 import { useGetEmissionsResultsByReportingPeriodQuery } from '@/redux/api/emission-results/emissionResultsApiSlice';
@@ -13,6 +15,7 @@ export function OrganizationUnitBannerContainer({
   reportingPeriodId,
   locale,
 }: OrganizationUnitBannerContainerProps) {
+  const { t } = useTranslation();
   const emissionResults = useGetEmissionsResultsByReportingPeriodQuery(
     {
       reportingPeriodId: reportingPeriodId ?? -1,
@@ -24,18 +27,25 @@ export function OrganizationUnitBannerContainer({
   );
 
   return (
-    <div className="flex flex-col gap-4">
-      {emissionResults.currentData &&
-        emissionResults.currentData.organizationUnits.map(
-          (orgUnitmissionResults) => (
-            <OrganizationUnitBanner
-              key={`org-unit-banner-${orgUnitmissionResults.id}`}
-              reportingPeriodId={reportingPeriodId}
-              locale={locale}
-              organizationUnitEmissionResults={orgUnitmissionResults}
-            />
-          ),
-        )}
+    <div className="border-t">
+      {emissionResults.currentData && (
+        <h2 className="break-normal my-10 px-2 sm:px-8">
+          {t('results.GHGEmissionsOfEachUnit')}
+        </h2>
+      )}
+      <div className="flex flex-col gap-4">
+        {emissionResults.currentData &&
+          emissionResults.currentData.organizationUnits.map(
+            (orgUnitmissionResults) => (
+              <OrganizationUnitBanner
+                key={`org-unit-banner-${orgUnitmissionResults.id}`}
+                reportingPeriodId={reportingPeriodId}
+                locale={locale}
+                organizationUnitEmissionResults={orgUnitmissionResults}
+              />
+            ),
+          )}
+      </div>
     </div>
   );
 }
