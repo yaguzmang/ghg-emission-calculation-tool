457f32d4a702de1ef72450ea4afe70d23096ae7d
Joonas Nuutinen <joonas.nuutinen@columbiaroad.com>
2023-08-24 10:54:04 +0300

    Create a skeleton controller for translations/export

--- backend/src/api/translation/controllers/translation.ts
+++ backend/src/api/translation/controllers/translation.ts
@@ -3,5 +3,103 @@
  */
 
 import { factories } from "@strapi/strapi";
+import utils from "@strapi/utils";
+import * as yup from "yup";
+import { validate } from "../../../services/utils";
+import { TranslatableEntry, TranslationService } from "../services/translation";
 
-export default factories.createCoreController("api::translation.translation");
+const { ApplicationError, ValidationError } = utils.errors;
+
+interface ExportableAttributes {
+  uid: string;
+  id: number;
+  attribute: string;
+  value_en: string;
+}
+
+/**
+ * Convert a content entry to an array of exportable attributes
+ * @param entry {TranslatableEntry} The entry to convert
+ * @returns {ExportableAttributes[]}
+ */
+const convertEntryToExportableAttributes = (
+  entry: TranslatableEntry
+): ExportableAttributes[] => {
+  // TODO
+  return [];
+};
+
+/**
+ * Convert a JSON-compliant JavaScript array of objects to a specified data format
+ * @param input {object[]} And array of objects to convert
+ * @param format {string} The output format
+ * @returns {string} The converted output
+ */
+const convertJsonTo = (input: object[], format: string): string => {
+  // TODO
+  return "";
+};
+
+export default factories.createCoreController(
+  "api::translation.translation",
+  ({ strapi }) => ({
+    async exportTranslations(ctx) {
+      // Validate request and extract typed arguments
+      const { as } = await validate(
+        ctx.query,
+        yup.object({ as: yup.string().required().oneOf(["csv"]) }),
+        ValidationError
+      );
+
+      // For each translatable uid, collect each translatable attribute
+      const translatableUids = [
+        "api:::emission-category.emission-category",
+        "api:::emission-group.emission-group",
+        "api:::emission-source-group.emission-source-group",
+        "api:::translation.translation",
+        "api:::settings-general.settings-general",
+      ];
+
+      const getTranslatableContentType = strapi.service<TranslationService>(
+        "api::translation.translation"
+      )?.getTranslatableContentType;
+
+      if (!getTranslatableContentType)
+        throw new ApplicationError(
+          "getTranslatableContentType service not available"
+        );
+
+      const translatableContentTypes = await Promise.all(
+        translatableUids.map(getTranslatableContentType)
+      );
+
+      // Find all entries of each translatable content type, selecting/populating the translatable attributes
+      const getTranslatableEntries = strapi.service<TranslationService>(
+        "api::translation.translation"
+      )?.getTranslatableEntries;
+
+      if (!getTranslatableEntries)
+        throw new ApplicationError(
+          "getTranslatableEntries service not available"
+        );
+
+      const translatableEntries = (
+        await Promise.all(
+          translatableUids.map((uid) =>
+            getTranslatableEntries(uid, translatableContentTypes[uid])
+          )
+        )
+      ).flatMap((_) => _);
+
+      // Loop through each entry's each attribute and build the exportable array of objects
+      const translatableAttributes = translatableEntries.flatMap(
+        convertEntryToExportableAttributes
+      );
+
+      // Transfer the json to csv and initiate a download
+      const output = convertJsonTo(translatableAttributes, as);
+
+      return output;
+    },
+  })
+);
--- backend/src/api/translation/services/translation.ts
+++ backend/src/api/translation/services/translation.ts
@@ -3,5 +3,57 @@
  */
 
 import { factories } from "@strapi/strapi";
+import { LocalizedApiServiceEntry } from "../../api.types";
 
-export default factories.createCoreService("api::translation.translation");
+interface TranslatableContentType {
+  kind: "collectionType" | "singleType";
+  attributes: {
+    [key: string]: {
+      type: "string" | "richtext" | "component";
+    };
+  };
+}
+
+interface TranslatableComponent {
+  [key: string]: string;
+}
+
+export type TranslatableEntry = {
+  [key: string]: string | TranslatableComponent;
+};
+
+export type TranslationService = {
+  getTranslatableContentType(
+    uid: string
+  ): Promise<TranslatableContentType | null>;
+  getTranslatableEntries(
+    uid: string,
+    schema: TranslatableContentType
+  ): Promise<TranslatableEntry[]>;
+};
+
+export default factories.createCoreService<
+  "api::translation.translation",
+  TranslationService
+>("api::translation.translation", ({ strapi }) => ({
+  /**
+   * Get a translatable content type by Strapi uid
+   * @param uid {string} The Strapi uid of the content type
+   * @returns {Promise<TranslatableContentType|null>} depending on whether a content type was found for the uid or not
+   */
+  async getTranslatableContentType(uid) {
+    // TODO
+    return null;
+  },
+
+  /**
+   * Get translatable entries of a content type
+   * @param uid {string} The Strapi uid of the content type
+   * @param schema {TranslatableContentType} The translatable content type schema
+   * @returns {Promise<TranslatableEntry[]>}
+   */
+  async getTranslatableEntries(uid, schema) {
+    // TODO
+    return [];
+  },
+}));
