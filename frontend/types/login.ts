eb9b0a3d16ee0da37ce85034dd2a85b60cc58a4a
yaguzmang <alejandroguzman.dev@gmail.com>
2023-08-27 22:19:49 +0200

    Add link to terms of service in the login page

--- frontend/types/login.ts
+++ frontend/types/login.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export type LoginData = {
   email: string;
   password: string;
+  termsOfService: boolean;
 };
 
 export const LoginSchema: z.ZodType<LoginData> = z.object({
@@ -12,5 +13,8 @@ export const LoginSchema: z.ZodType<LoginData> = z.object({
     .email('login.error.email.valid'),
   password: z.string().min(1, {
     message: 'login.error.password.input',
+  }),
+  termsOfService: z.literal<boolean>(true, {
+    errorMap: () => ({ message: 'login.error.termsOfService.accept' }),
   }),
 });
